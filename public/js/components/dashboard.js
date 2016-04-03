40c3c9a62bad13a7f9abe7be8b09465e519421bf
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-04-03 14:34:10 -0400

    bug fixing yummly id request

--- public/js/components/dashboard.js
+++ public/js/components/dashboard.js
@@ -18,189 +18,10 @@ const Dashboard = React.createClass({
   },
   componentDidMount : function() {
 
-  var content = [
-    { title: "Bread"},
-    { title: "Eggs"},
-    { title: "Milk"},
-    { title: "Vanilla extract"},
-    { title: "Raw sugar"},
-    { title: "Ground cinnamon"},
-    { title: "Butter"},
-    { title: "Bone in skin on chicken thigh"},
-    { title: "Baby potatoes"},
-    { title: "Baby carrots"},
-    { title: "Olive oil"},
-    { title: "Salt"},
-    { title: "Pepper"},
-    { title: "Unsweetened applesauce"},
-    { title: "Granulated sugar"},
-    { title: "Large eggs"},
-    { title: "All-purpose flour"},
-    { title: "Baking soda"},
-    { title: "Baking powder"},
-    { title: "Cinnamon"},
-    { title: "Carrots"},
-    { title: "Cream cheese"},
-    { title: "Almond extract"},
-    { title: "Powdered sugar"},
-    { title: "Chicken breasts"},
-    { title: "Taco seasoning"},
-    { title: "Plain greek yogurt"},
-    { title: "Black beans"},
-    { title: "Salsa"},
-    { title: "Mexican cheese blend"},
-    { title: "Tortilla chips"},
-    { title: "Almond butter"},
-    { title: "Semisweet chocolate"},
-    { title: "White chocolate"},
-    { title: "Extra-virgin olive oil"},
-    { title: "Pure maple syrup"},
-    { title: "Apple cider vinegar"},
-    { title: "Grated lemon zest"},
-    { title: "Lemon juice"},
-    { title: "Kosher salt"},
-    { title: "Black pepper"},
-    { title: "Warm water"},
-    { title: "Instant yeast"},
-    { title: "Canola oil"},
-    { title: "Drummettes"},
-    { title: "Melted butter"},
-    { title: "Paprika"},
-    { title: "Onion powder"},
-    { title: "Honey"},
-    { title: "Sriracha"},
-    { title: "Lime juice"},
-    { title: "Soy sauce"},
-    { title: "Brownie mix"},
-    { title: "Light corn syrup"},
-    { title: "Salted butter"},
-    { title: "Heavy cream"},
-    { title: "Frosting"},
-    { title: "Unsweetened cocoa powder"},
-    { title: "Whole milk"},
-    { title: "Salmon fillets"},
-    { title: "Garlic"},
-    { title: "Chopped parsley"},
-    { title: "Parmesan cheese"},
-    { title: "Sliced potatoes"},
-    { title: "Flour"},
-    { title: "Cayenne pepper"},
-    { title: "Sharp cheddar cheese"},
-    { title: "Grating cheese"},
-    { title: "Lean ground pork"},
-    { title: "Lean ground beef"},
-    { title: "Sesame oil"},
-    { title: "Panko breadcrumbs"},
-    { title: "Ginger paste"},
-    { title: "Minced garlic"},
-    { title: "Green onions"},
-    { title: "Lemongrass"},
-    { title: "Marinade"},
-    { title: "Sesame seeds"},
-    { title: "Chicken legs"},
-    { title: "Tamari soy sauce"},
-    { title: "Hoisin sauce"},
-    { title: "Chinese mustard"},
-    { title: "Orange juice"},
-    { title: "Dijon mustard"},
-    { title: "Garlic cloves"},
-    { title: "Sea salt"},
-    { title: "Meat"},
-    { title: "Onions"},
-    { title: "Fresh ginger"},
-    { title: "Rice vinegar"},
-    { title: "Asian chili sauce"},
-    { title: "Dark sesame oil"},
-    { title: "Lettuce leaves"},
-    { title: "Low sodium soy sauce"},
-    { title: "Water"},
-    { title: "Ginger"},
-    { title: "Chili paste"},
-    { title: "Lime"},
-    { title: "Corn starch"},
-    { title: "Ground chicken"},
-    { title: "Salted cashews"},
-    { title: "Lettuce"},
-    { title: "Cilantro"},
-    { title: "Hellmann real mayonnais"},
-    { title: "Sugar"},
-    { title: "Ground ginger"},
-    { title: "Slaw mix"},
-    { title: "Chopped fresh cilantro"},
-    { title: "Chicken drumsticks"},
-    { title: "Olive oil spray"},
-    { title: "Balsamic vinegar"},
-    { title: "Chives"},
-    { title: "Ground black pepper"},
-    { title: "Wish bone western origin dress"},
-    { title: "Bacon, crisp-cooked and crumbled"},
-    { title: "Tomatoes"},
-    { title: "Coleslaw"},
-    { title: "Unsalted butter"},
-    { title: "Green bell pepper"},
-    { title: "Ham"},
-    { title: "Iceberg lettuce"},
-    { title: "Frozen whole kernel corn"},
-    { title: "Cucumber"},
-    { title: "Purple onion"},
-    { title: "Shredded Monterey Jack cheese"},
-    { title: "Wish bone western dress"},
-    { title: "Seasoning salt"},
-    { title: "Cooked ham"},
-    { title: "Shredded cheddar cheese"},
-    { title: "Finely chopped onion"},
-    { title: "Chopped green bell pepper"},
-    { title: "Red bell pepper"},
-    { title: "Fresh chives"},
-    { title: "Chopped onion"},
-    { title: "Chopped bell pepper"},
-    { title: "Deli ham"},
-    { title: "Chopped ham"},
-    { title: "Green pepper"},
-    { title: "Cheese"},
-    { title: "Pork shoulder"},
-    { title: "Barbecue rub"},
-    { title: "Barbecue sauce"},
-    { title: "Sweet onion"},
-    { title: "Ground beef"},
-    { title: "Wagon wheel pasta, cook and drain"},
-    { title: "Chili powder"},
-    { title: "Cumin seed"},
-    { title: "Chile pepper"},
-    { title: "Ground turmeric"},
-    { title: "Mustard greens"},
-    { title: "Fresh spinach"},
-    { title: "Ground cumin"},
-    { title: "Ground coriander"},
-    { title: "Potatoes"},
-    { title: "Ghee"},
-    { title: "Ginger root"},
-    { title: "Stew"},
-    { title: "Coriander"},
-    { title: "Amchur"},
-    { title: "Green chilies"},
-    { title: "Garam masala"},
-    { title: "Turmeric"},
-    { title: "Red chili powder"},
-    { title: "Safflower oil"},
-    { title: "Yellow onion"},
-    { title: "Dried mint"},
-    { title: "Cauliflower florets"},
-    { title: "Low sodium vegetable broth"},
-    { title: "Quick cooking brown rice"},
-    { title: "Currant"},
-    { title: "Frozen peas"},
-    { title: "Lentils"},
-    { title: "Unsalted almonds"},
-    { title: "Low-fat plain yogurt"},
-    { title: "Cauliflower"},
-    { title: "Oil"},
-    ];
-
-    $('.ui.search')
-    .search({
-      source: content
-    });
+    // $('.ui.search')
+    // .search({
+    //   source: content
+    // });
 
   },
   handleSubmit : function(event) {
@@ -366,6 +187,7 @@ const RecipeResult = React.createClass({
     }
   },
   componentWillMount : function() {
+    console.log('grabing recipe id details');
     $.ajax({
       url: '/api/users/yummly/'+ this.props.details.id,
       type: 'GET',
@@ -384,22 +206,48 @@ const RecipeResult = React.createClass({
     });
   },
   componentDidMount : function() {
+    let that = this;
     $(".rating").rating();
+
+    $('.recipeImg').on('load', function() {
+      that.updateState();
+    })
+
   },
   handleClick : function() {
     $('.ui.modal.'+this.props.index)
     .modal('show')
+  },
+  updateState : function() {
+    console.log('updating state');
+    $.ajax({
+      url: '/api/users/yummly/'+ this.props.details.id,
+      type: 'GET',
+      beforeSend: function( xhr ) {
+        xhr.setRequestHeader("Authorization", 'Bearer ' + auth.getToken() );
+      }
+    })
+    .done((data) => {
+      let parsedData = JSON.parse(data);
+      this.setState({
+        recipeImage: parsedData.images[0].hostedLargeUrl,
+        name: parsedData.name,
+        ingredients: parsedData.ingredientLines,
+        totalTime: parsedData.totalTime
+      });
+    });
   },
   render : function() {
     return (
 
         <div className="card" onClick={this.handleClick}>
           <div className="image">
-            <img src={this.state.recipeImage} />
+            <img src={this.props.details.smallImageUrls[0]} className="recipeImg" />
           </div>
           <div className="extra">
             Rating:
-            <div className="ui star rating" data-rating={this.props.details.rating}></div>
+            <div className="ui star rating" data-rating={this.props.details.rating}></div><br/>
+            {this.props.details.recipeName}
           </div>
 
           <div className={"ui modal "+this.props.index}>
--- routes/users.js
+++ routes/users.js
@@ -18,8 +18,6 @@ users.use(function(error, request, response, next) {
 users.post('/yummly', (req, res) => {
   let allowed = req.body.qs.split('+');
   allowed = allowed.join('&allowedIngredient[]=');
-  console.log(allowed);
-
 
   request(`http:/\/api.yummly.com/v1/api/recipes?_app_id=${process.env.YUMMLY_APP_ID}&_app_key=${process.env.YUMMLY_APIKEY}&allowedIngredient[]=${allowed}&maxResult=16`, function (error, response, body) {
     if (!error && response.statusCode == 200) {
