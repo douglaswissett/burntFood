45212d76db325044e4f63351f91555b9fef90706
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-04-11 08:54:58 -0400

    herko configs

--- db/pg/recipes.js
+++ db/pg/recipes.js
@@ -1,14 +1,17 @@
 'use strict';
 const pgp = require('pg-promise')({});
 
-
-  const cn = {
-      host: process.env.HOST, // server name or IP address;
+if(process.env.ENVIRONMENT === 'production') {
+  var cn = process.env.DATABASE_URL;
+} else {
+  var cn = {
+      host: 'localhost', // server name or IP address;
       port: 5432,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASS
-  }  
+  };
+}
 
 
 const db = pgp(cn)
