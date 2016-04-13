5436006d4fb0eeb0a7690f80f64fbc32239c0c54
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-04-13 13:22:26 +0200

    removed heroku configs

--- db/pg/recipes.js
+++ db/pg/recipes.js
@@ -1,18 +1,13 @@
 'use strict';
 const pgp = require('pg-promise')({});
 
-if(process.env.ENVIRONMENT === 'production') {
-  var cn = process.env.DATABASE_URL;
-} else {
-  var cn = {
-      host: 'localhost', // server name or IP address;
-      port: 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS
-  };
-}
-
+var cn = {
+    host: 'localhost', // server name or IP address;
+    port: 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+};
 
 const db = pgp(cn)
 
--- db/pg/users.js
+++ db/pg/users.js
@@ -2,17 +2,13 @@
 const pgp = require('pg-promise')({});
 
 
-if(process.env.ENVIRONMENT === 'production') {
-  var cn = process.env.DATABASE_URL;
-} else {
-  var cn = {
-      host: 'localhost', // server name or IP address;
-      port: 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS
-  };
-}
+var cn = {
+    host: 'localhost', // server name or IP address;
+    port: 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+};
 
 
 
